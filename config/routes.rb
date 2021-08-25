146ce80a0e73b94ac56abf67f1ca5ef20e16cb8b
NTN <ntn201@gmail.com>
2021-08-25 19:09:37 +0700

    Fix omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,7 @@ Rails.application.routes.draw do
   resources :friend_ships, only: [:create]
   resources :friend_requests, only: [:create]
 
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations'
-  # }
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
-  # devise_scope :user do
-  #   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
 
   resources :users, only: [:show,:index] do
     resources :photos, only: [:new, :create, :edit, :destroy]
